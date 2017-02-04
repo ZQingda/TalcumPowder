73a4986b5e26b6d126cfc0231079ea54c6010380
Michelle Zhong <y45zhong@edu.uwaterloo.ca>
2017-02-04 14:58:41 -0500

    Add a named tuple

--- talk.py
+++ talk.py
@@ -1,2 +1,10 @@
+import collections
+
+#Named Tuple for the Patient Data
+
+PatientData = collections.namedtuple('PatientData', 'PatientID PatientName Condition Medicine Volume VolumeUnit Frequency FrequencyUnit OtherDetails')
+
+joe = PatientData(PatientID='1208205005893863', PatientName='joe', Condition='Diabetes', Medicine='Insulin', Volume=100, VolumeUnit='units per mL', Frequency='1', FrequencyUnit='Day', OtherDetails='Eat before meals')
+
 def tester(say):
     return "How about this!"

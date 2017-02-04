33b2e2a9cbc75fafcf17d292903c1c475817828e
Michelle <lelehcim@gmail.com>
2017-02-04 20:21:03 +0000

    Moved data to exterior file

--- talk.py
+++ talk.py
@@ -1,12 +1,5 @@
 import collections
-
-#Named Tuple for the Patient Data
-
-PatientData = collections.namedtuple('PatientData', 'PatientID PatientName Condition Medicine Volume VolumeUnit Frequency FrequencyUnit OtherDetails')
-
-joe = PatientData(PatientID='1208205005893863', PatientName='joe', Condition='Diabetes', Medicine='Insulin', Volume=100, VolumeUnit='units per mL', Frequency='1', FrequencyUnit='Day', OtherDetails='Eat before meals')
-
-PatientArray = [joe]
+import data
 
 def tester(say):
     return "How about this!"

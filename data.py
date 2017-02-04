f17052b8b4b6f2d907b6f20c73e33b25545676aa
Michelle <lelehcim@gmail.com>
2017-02-04 20:36:28 +0000

    Changed to recordclass instead of named tuples

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-import collections
+import recordclass
 
 #Named Tuple for the Patient Data
 
-PatientData = collections.namedtuple('PatientData', 'PatientID PatientName Condition Medicine Volume VolumeUnit Frequency FrequencyUnit HasLog OtherDetails')
+PatientData = recordclass('PatientData', 'PatientID PatientName Condition Medicine Volume VolumeUnit Frequency FrequencyUnit HasLog OtherDetails')
 
 joe = PatientData(PatientID='1208205005893863', PatientName='joe', Condition='Diabetes', Medicine='Insulin', Volume=100, VolumeUnit='units per mL', Frequency='1', FrequencyUnit='Day', HasLog='Y', OtherDetails='Eat before meals')
 

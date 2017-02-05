506ea7df00df0ddf3105407902eaf8ed373dc67e
Michelle <lelehcim@gmail.com>
2017-02-05 04:35:41 +0000

    Edited text of messages

--- data.py
+++ data.py
@@ -4,12 +4,17 @@ from recordclass import recordclass
 
 PatientData = recordclass('PatientData', 'PatientID PatientName Condition Medicine Volume VolumeUnit Frequency FrequencyUnit HasTakenDrug OtherDetails')
 
-joe = PatientData(PatientID='1208205005893863', PatientName='joe', Condition='Diabetes', Medicine='Insulin', Volume=100, VolumeUnit='units per mL', Frequency='1', FrequencyUnit='Day', HasTakenDrug=False, OtherDetails='Eat before meals')
+joe = PatientData(PatientID='1208205005893863', PatientName='Joe', Condition='Diabetes', Medicine='Insulin', Volume=100, VolumeUnit='units per mL', Frequency='1', FrequencyUnit='Day', HasTakenDrug=False, OtherDetails='Eat before meals')
 
 patients = [joe]
 
 def getData(name):
     return patients[name]
+
+def addPatient(id, name, condition, medicine, volume, volumeUnit, freq, freqUnit, hasTakenDrug, otherDetails):
+    patient = PatientData(id, name, condition, medicine, volume, volumeUnit, freq, freqUnit, hasTakenDrug, otherDetails)
+    patients.append(patient)
+    return "You have been added to the database."
 
 doctorName = "Dr.Stein"
 

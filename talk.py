80f8c08ea464b7af3043b532a665becb9e116bbc
Michelle Zhong <y45zhong@edu.uwaterloo.ca>
2017-02-04 21:32:04 -0500

    add better response logic

--- talk.py
+++ talk.py
@@ -1,36 +1,38 @@
 import data
 
-def tester(say):
+def tester(say, senderID):
     #check if id exists in the db array
 
     for i in data.patients:
-        if i.PatientID == -1:
-            return getIntro(i.PatientName, data.doctorName, i.Condition)
-    #if the id doesn't exist, give the getIntro
-        elif i.PatientID != -1:
-    #if the id exists, look at the "say"
+        if i.PatientID == senderID:
             if say == data.drugTaken:
                 i.HasTakenDrug=True
                 return getHasTakenDrug(i.PatientName)
             elif say == "Details":
                 return getDetails(i.Condition, str(i.Volume) + " " + i.VolumeUnit, str(i.Frequency) + " " + i.FrequencyUnit)
+            else:
+                 return "To log a drug taken, type \"Drug Taken\". For more details, type \"Details\" :)"
         else:
-            return data.invalidCommand
+            return getIntroNew(" ")
+        
 
     #turn "drugTaken" into "true" 
     #get getDetails if asked for Detailed
     #else turn into "invalid command, try again" 
-    return ":) :)!"
+    return "To log a drug taken, type \"Drug Taken\". For more details, type \"Details\" :)"
 
     
 #def tester(say):
 #    return "How about this! " + say + " "
 
+def getIntroNew(useless):
+    return 'Hey! I am your medicine Manager, assigned by Dr.%s. I see that you are new here! Please talk to the Doctor to get a prescription.'% s(data.doctorName)
+
 def getHasTakenDrug(username):
     return 'You have taken your medicine, %s, great!' % (username)
 
 def getIntro(username, doctorName, condition):
-    return 'Hey %s! I am your medicine manager assigned by Dr.%s. You have been prescribed medicine for %s. You will be reminded by me to take your medicine at timed intervals your doctor has provided me. To list the details of this prescription, send <details>. For more information send <help>.' % s(username, doctorName, condition)
+    return 'Hey %s! I am your medicine manager assigned by Dr.%s. You have been prescribed medicine for %s. You will be reminded by me to take your medicine at timed intervals your doctor has provided me. To list the details of this prescription, send "Details".' % s(username, doctorName, condition)
 
 def getDetails(condition, dosage, interval):
     return 'Your have been prescribed medicine for %s. You will be taking %s %s. For details on how to take your medicine, send <details>.' % (condition, dosage, interval)

87819eb96b3661640e2e01b0537482552f12967e
Haoyang <Haoyang>
2017-02-04 19:45:41 -0500

    fs

--- talk.py
+++ talk.py
@@ -3,14 +3,16 @@ import data
 def tester(say):
     return "How about this!"
 
+    
+def tester(say):
+    return "How about this! " + say + " "
+
+
 def getIntro(username, doctorName, condition):
     return 'Hey {}! I am your medicine manager assigned by Dr.{}. You have been prescribed medicine for {}. You will be reminded by me to take your medicine at timed intervals your doctor has provided me. To list the details of this prescription, send <details>. For more information send <help>.'.format(username, doctorName, condition)
 
 def getDetails(condition, dosage, interval):
     return 'Your have been prescribed medicine for {}. You will be taking {} {}. For details on how to take your medicine, send <details>.'.format(condition, dosage, interval)
-
-def tester(say):
-    return "How about this! " + say + " "
 
 def getReminder(username, dosage):
     return 'Hello {}! Reminder that you need to take {} now!'.format(username, dosage)

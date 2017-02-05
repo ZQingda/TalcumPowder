b0eb7a5ba3e746867cce8bfe27cbba79ea0208a4
Michelle Zhong <y45zhong@edu.uwaterloo.ca>
2017-02-04 21:59:02 -0500

    fix formatting

--- talk.py
+++ talk.py
@@ -15,22 +15,22 @@ def tester(say, senderID):
     #turn "drugTaken" into "true" 
     #get getDetails if asked for Detailed
     #else turn into "invalid command, try again" 
-    return getIntroNew(" ")
+    return "Hey new guy. Useless." #getIntroNew(" ")
     
 #def tester(say):
 #    return "How about this! " + say + " "
 
 def getIntroNew(useless):
-    return 'Hey! I am your medicine Manager, assigned by Dr.%s. I see that you are new here! Please talk to the Doctor to get a prescription.'% s(data.doctorName)
+    return 'Hey! I am your medicine Manager, assigned by Dr. %s. I see that you are new here! Please talk to the Doctor to get a prescription.'% (data.doctorName)
 
 def getHasTakenDrug(username):
     return 'You have taken your medicine, %s, great!' % (username)
 
 def getIntro(username, doctorName, condition):
-    return 'Hey %s! I am your medicine manager assigned by Dr.%s. You have been prescribed medicine for %s. You will be reminded by me to take your medicine at timed intervals your doctor has provided me. To list the details of this prescription, send "Details".' % s(username, doctorName, condition)
+    return 'Hey %s! I am your medicine manager assigned by Dr.%s. You have been prescribed medicine for %s. You will be reminded by me to take your medicine at timed intervals your doctor has provided me. To list the details of this prescription, send "Details".' % (username, doctorName, condition)
 
 def getDetails(condition, dosage, interval):
-    return 'Your have been prescribed medicine for %s. You will be taking %s %s. For details on how to take your medicine, send <details>.' % (condition, dosage, interval)
+    return 'Your have been prescribed medicine for %s. You will be taking %s %s.' % (condition, dosage, interval)
 
 def getReminder(username, dosage):
     return 'Hello %s! Reminder that you need to take %s now!' % (username, dosage)
